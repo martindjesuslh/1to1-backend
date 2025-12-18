f987ece0de8f2b47696e78b8f427d1d95fe6b616
Martin De Jesus <martindjesuslh@gmail.com>
2025-12-17 19:10:36 -0600

    feat: add CORS configuration for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,11 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 
   console.log(`Application is running on: http:localhost:${process.env.PORT ?? 3000}`);
